db134366724c7ba7f310bb075a6f9e2c753dae31
kaichen <kaichenc@andrew.cmu.edu>
2018-10-14 18:11:13 -0400

    Add Exmaple Interface to better search for examples.

--- src/BasicExample/OOPExample.java
+++ src/BasicExample/OOPExample.java
@@ -6,6 +6,29 @@ public class OOPExample {
     }
 }
 
+
+class AbstractFinalExample {
+    public static void main(String [] arg){
+        AbClass.aa();
+    }
+}
+
+
+abstract class AbClass{
+    short a = 1;
+    abstract void abc();
+    static void aa(){ System.out.println("Suprise!?!?"); };
+//    abstract static void bb();
+    public static void main(String[] arg){}
+}
+
+//class IWanttoHaveAbstractMethod{
+//    abstract void someMethod(){
+//
+//    }
+//}
+
+
 class PrivateClass{
     public static void method1(){
 
@@ -23,20 +46,20 @@ interface Examplifiable{
     // 4. final var should be initialized
     public static final int EXAMPLE_NUM = 0;
 
-    // 1. could be public / default
+    // 1. Could be public / default
     // 2. private method must be implemented HERE
     //    static method also MUST be implemented
     // 3. cannot be protected
     // 4. "public" is redundant
     public void run();
     void run_inpackage();
-    private void running(){}
 
+    private void running(){}
     public static void staticRun(){};
 }
 
 class Examplification implements Examplifiable {
-    // Must implements all non-private method
+    // Must implements ALL non-private method
     @Override
     public void run() {}
 
@@ -56,3 +79,10 @@ class Figures extends Examplification{
         }
     }
 }
+
+
+class InnerClassExample {
+
+}
+
+
