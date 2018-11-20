336ccb39ed9678484ce0f31eaedfd80d65eac3dd
kaichen <kaichenc@andrew.cmu.edu>
2018-11-20 07:36:18 -0500

    backup

--- src/ZDraft/Practice1108.java
+++ src/ZDraft/Practice1108.java
@@ -1,5 +1,12 @@
 package ZDraft;
 
+import javafx.beans.binding.DoubleBinding;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class Practice1108 {
     public static void main(String[] args){
         String s1 = "1";
@@ -29,5 +36,62 @@ public class Practice1108 {
         // but the presentation change,
         // e.g., char 49 present as '1'
         //       int 49 present as 49
+    }
+}
+
+class Practice1119{
+
+    public static void main(String[] args){
+        String s1 = "1";
+        int i1 = Integer.parseInt(s1);
+        char c1 = (char)(i1+48);
+        System.out.println(c1);
+        Character c2 = new Character(c1);
+        String s2 = String.valueOf(i1);
+        Double d1 = Double.valueOf("2.2");
+        System.out.println(d1);
+        String s3 = String.valueOf(d1);
+        String s4 = String.valueOf(i1);
+        String s5 = new String(new char[]{(char)(i1+48)});
+        char c3 = ++c2;
+        String[] sl1 = s3.split("\\s");
+        String[] sl2 = s3.split("\n");
+        String[] sl3 = s3.split("\\.");
+        for(String s: sl3){
+            System.out.println(s);
+        }
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(s1);
+        sb1.append("\n");
+        sb1.append(s2);
+        String s6 =sb1.toString();
+        sl3 = s6.split("\n");
+        for(String s: sl3){
+            System.out.println(s);
+        }
+        System.out.println(i1==((char)i1));
+        System.out.println(i1==c1);
+        System.out.println("a".compareTo("A"));
+        System.out.println("aa".compareTo("AA"));
+        System.out.println("aa".compareTo("AZ"));
+        char c4 = 'a';
+        c4 = String.valueOf(c4).toUpperCase().toCharArray()[0];
+        c4 = Character.toUpperCase(c4);
+        List<Integer> l1 = new ArrayList<>();
+        class Foo implements Comparable<Foo>{
+            int id;
+            Foo(int i){ id = i; }
+            @Override
+            public int compareTo(Foo o) {
+                return -(o.id - id);
+            }
+        }
+        List<Foo> l2 = new ArrayList<>();
+        l2.add(new Foo(2)); l2.add(new Foo(1));
+        Collections.sort(l2);
+        Iterator<Foo> iter = l2.iterator();
+        while(iter.hasNext()){
+            System.out.println(iter.next().id);
+        }
     }
 }
--- src/ZDraft/ZDraft0902.java
+++ src/ZDraft/ZDraft0902.java
@@ -2,7 +2,9 @@ package ZDraft;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class ZDraft0902 {
     public static void main(String arg[]){
@@ -56,4 +58,80 @@ class ZDraft1107{
 
     }
 }
+
+
+class ZDraft0929 {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("1");
+        sb.insert(0, "A");
+        sb.insert(0, 1.2);
+        System.out.println(sb.toString());
+        String s = "1";
+        System.out.println(s.charAt(0)-48);
+        System.out.println();
+
+        char c1 = '1';
+        char c2 = '2';
+        char c3 = (char)(c1+c2-48);
+        System.out.println(c3);
+        System.out.println((char)(c3+1));
+        System.out.println(c3>'9');
+        System.out.println((char)(c3-10));
+    }
+}
+
+class Draft1028{
+    public static void main(String[] args){
+        Set<String> s = new HashSet<>();
+        System.out.println("");
+    }
+}
+
+class ChineseChar{
+    private StringBuilder sb = new StringBuilder("");
+    private String[] strArr;
+    void Seperation(){
+        Scanner input;
+        input = new Scanner(System.in);
+        while(input.hasNextLine()) {
+            sb.append(input.nextLine());
+            sb.append("\n");
+        }
+        if(sb.length()==0) return;
+        strArr = sb.toString().split("\n");
+
+        for(String line: strArr){
+
+        }
+
+    }
+
+}
+
+
+class Draft1004{
+    public static void main(String[] args){
+        Draft1004 d = new Draft1004();
+        System.out.println(d.findGCD(12,16));
+    }
+
+    public int findGCD(int n, int d){
+        if(n==0) return 1;
+        if(d==0) return n;
+        else findGCD(d, n%d);
+        return 0;
+    }
+}
+
+
+
+class Draft1007 {
+    public static void main(String[]args){
+//        "abc".toChar
+        String abc = "abc";
+        abc = abc + "d";
+        System.out.printf(abc);
+    }
+}
+
 
