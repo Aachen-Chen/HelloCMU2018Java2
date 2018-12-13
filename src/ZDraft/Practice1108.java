919bf02bdfda37bbe921c665d6e8da564d5deab4
kaichen <kaichenc@andrew.cmu.edu>
2018-12-13 17:00:10 -0500

    practice 1213

--- src/ZDraft/Practice1108.java
+++ src/ZDraft/Practice1108.java
@@ -156,3 +156,105 @@ class Practice1121{
         Collections.sort(ill);
     }
 }
+
+class Practice1213{
+    public static void main(String[] args){
+        String s = "1";
+        // TODO: String to int
+        int i = Integer.parseInt(s);
+
+        // TODO: Int to char.
+        char c = (char)i;
+        System.out.println((char)((int)'0'+c)); //1213 bet on '1'.
+
+        // TODO: Char to Character
+        Character cc = c;
+
+        // TODO: How can we achieve auto-boxing and auto-unboxing:
+        // like above should be fine.
+
+        // TODO: Can Wrapper ++? Can char / Character ++? Prove with only one example.
+        Character c2 = ++c;
+        char c3 = ++c2;
+
+        // TODO: any type to String         // 1213 String's valueOf use both its constructor and...
+        String s2 = String.valueOf(cc);
+
+        // TODO: any type to any type's Wrapper (String to Integer)
+//        Integer i2 = Integer.valueOf(s2);     // 1213 this can't be done 'cause s2 is not a number in ascii.
+            // 1213 thus, remember: int->char pass value, String->int pass meaning
+        Integer i3 = Integer.valueOf(cc);
+        Integer i2 = Integer.valueOf(c);
+//        Integer i5 = Integer.valueOf(new Double(2.22));   // Unless involve precision.
+
+        // TODO: any wrapper type to String
+        String s3 = String.valueOf(i2);
+        String s4 = String.valueOf(new Double(0.11));
+
+        // TODO: any primitive type to String
+        String s5 = String.valueOf(0.2334f);
+
+        // TODO: what is the difference between String() and String.valueOf(): see it!
+        // See 1121
+
+        // TODO: Turn char[] into String
+        String s6 = new String(new char[]{'a','b','c'});
+
+        // TODO: Concatenate two string
+        String s7 = s6+s5;
+
+        // TODO: Sort string[], Z->A      //cao, bu hui le...
+        String[] abc = new String[]{"c", "b", "a"};
+        Arrays.sort(abc);
+        for(String sss: abc){System.out.println(sss);}
+        Arrays.sort(abc, ((o1, o2) -> {return o2.compareTo(o1);}));
+        Arrays.sort(abc, (Comparator.reverseOrder()));
+        for(String sss: abc){System.out.println(sss);}
+
+        // TODO: Append string and stringbuilder
+        String s11 = "abc";
+//        s11.append()
+
+        // TODO: Split a String using: white space, new line, tab, etc. "."
+        // todo: 1119 has better example.
+        StringBuilder sb = new StringBuilder();
+        sb.append("0\n1 2\t3.4");
+        String s8 = sb.toString();
+        String[] s10= new String[5];
+        String[] s9 = s8.split("\n");   s10[0] = s9[0];
+        s9 = s9[1].split("\\s");        s10[1] = s9[0];
+        s9 = s9[1].split("\t");         s10[2] = s9[0];
+//        s9 = s9[1].split("\\.");        s10[3] = s9[0]; s10[4]=s9[1];
+
+        // TODO: How do you assign value to char
+        char ccccc = 'c';
+
+        // TODO: If assign some value to char, will it print the value?
+        // 1213: no, it will be the ascii mark of that value.
+
+        // TODO: What is char == int
+        // 1213 i think they compare the value. todo: right.
+        int i4 = 48;
+        char c4 = (char)i4;
+        System.out.println(i4==c4);
+
+        // TODO: What is "a".compareTo("A")
+        // 1213: negative.
+        System.out.println("a".compareTo("A"));
+
+        // TODO: What is "cc".compareTo("ba")
+        // 1213: only the first character
+
+        // TODO: Turn char a = 'a' to upper case
+        char a = 'a'; a = Character.toUpperCase(a);
+
+        // TODO: Declare an array, initialize anonymously 1119 Really work?
+        int[] il = new int[] {1,2,3}; // 1213 I think this is anonymous declaration
+
+        // TODO: Sort an array. What is the compareTo, and how to change it? 1119 remember natural order!
+        // refer to above.
+
+        // 1213: total time: 1h.
+    }
+}
+
